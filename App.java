86233420dfd4a509343d6a8ff84184174a0e117b
init 5 <r00tsec@ya.ru>
2018-08-31 09:47:14 +0200

    Update App.java

--- App.java
+++ App.java
@@ -1,18 +1,10 @@
 package be.intecbrussel.lambda;
 
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.charset.Charset;
 import java.nio.file.FileSystem;
 import java.nio.file.FileSystems;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class fileio {
@@ -33,24 +25,27 @@ public class fileio {
 			System.out.println("Sucessfully created the directory.\n" + "Enter your txt file name: [without .txt]");
 			String filename = input.next();
 			File f = new File(home + "/" + filename + ".txt");
-			try {
-				FileOutputStream fs = new FileOutputStream(f);
-			} catch (FileNotFoundException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			Path path = Paths.get(home + "/Desktop/testFolder/" + filename + ".txt");
-			System.out.println("Enter your msg to " + filename);
-			String data = input.next();
-			List<String> lines = new ArrayList<>();
-			lines.add(data);
 
+			FileWriter writer = null;
 			try {
-				Files.write(path, lines, Charset.defaultCharset(), StandardOpenOption.APPEND);
-			} catch (IOException e) {
+				writer = new FileWriter(home + "/Desktop/testFolder/" + filename + ".txt");
+				System.out.println("Enter your msg to :");
+				String data = input.next();
+				writer.write(data);
+			} catch (IOException e1) {
 				// TODO Auto-generated catch block
-				e.printStackTrace();
+				e1.printStackTrace();
+			} finally {
+				if(writer !=null) {
+					try {
+						writer.close();
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
 			}
+
 		} else if (osNameMatch.contains("windows")) {
 			new File(home + "\\Desktop\\testFolder").mkdir();
 		} else if (osNameMatch.contains("solaris") || osNameMatch.contains("sunos")) {
